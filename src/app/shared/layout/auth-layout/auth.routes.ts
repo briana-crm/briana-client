5736962a6fdef7f20ed358ae7f150ce054c97a29
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2021-03-28 21:13:44 +0300

    feat: add base static component

--- src/app/shared/layout/auth-layout/auth.routes.ts
+++ src/app/shared/layout/auth-layout/auth.routes.ts
@@ -2,6 +2,5 @@ import {LoginPageComponent} from '../../../login-page/login-page.component';
 import {Routes} from '@angular/router';
 
 export const AUTH_ROUTES: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginPageComponent}
 ];
